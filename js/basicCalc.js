10eaa7b9faef9db0de820c1b121cec4472733424
Tech-A40 <Tech-A40@Tech-A40s-MacBook-Air.local>
2017-06-20 19:18:45 +0100

    Made all number buttons output value when clickedr

---
+++ js/basicCalc.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // your code here
+
+  var display = document.getElementsByClassName('display')[0];
+
+  var seven = document.getElementsByName('seven')[0];
+  var eight = document.getElementsByName('eight')[0];
+  var nine = document.getElementsByName('nine')[0];
+  var four = document.getElementsByName('four')[0];
+  var five = document.getElementsByName('five')[0];
+  var six = document.getElementsByName('six')[0];
+  var one = document.getElementsByName('one')[0];
+  var two = document.getElementsByName('two')[0];
+  var three = document.getElementsByName('three')[0];
+  var zero = document.getElementsByName('zero')[0];
+
+  seven.addEventListener('click', clickSeven);
+  eight.addEventListener('click', clickEight);
+  nine.addEventListener('click', clickNine);
+  four.addEventListener('click', clickFour);
+  five.addEventListener('click', clickFive);
+  six.addEventListener('click', clickSix);
+  one.addEventListener('click', clickOne);
+  two.addEventListener('click', clickTwo);
+  three.addEventListener('click', clickThree);
+  zero.addEventListener('click', clickZero);
+
+  function clickSeven(event) {
+  	display.value += this.value; 
+  }
+
+  function clickEight(event) {
+  	display.value += this.value;
+  }
+
+  function clickNine(event) {
+  	display.value += this.value;
+  }
+
+  function clickFour(event) {
+  	display.value += this.value;
+  }
+
+  function clickFive(event) {
+  	display.value += this.value;
+  }
+
+  function clickSix(event) {
+  	display.value += this.value;
+  }
+
+  function clickOne(event) {
+  	display.value += this.value;
+  }
+
+  function clickTwo(event) {
+  	display.value += this.value;
+  }
+
+  function clickThree(event) {
+  	display.value += this.value;
+  }
+
+  function clickZero(event) {
+  	display.value += this.value;
+  }
+
+
+
+
+
+});
